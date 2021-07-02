21d766175bf3cec4caa42366485483b643596b86
sahankj2000 <sahan.k.j2000@gmail.com>
2021-07-02 18:35:42 +0530

    added dark mode switching

--- old_stable.cpp
+++ old_stable.cpp
@@ -1,12 +1,10 @@
 #include<iostream>
 #include<GL/glut.h>
 #include<stdlib.h>
-#include<cstring>
 using namespace std;
 
 int menuInt=0;
 int gameInt=0;
-int score=0;
 bool paused=false;
 const int R=30;  // Till R=80 and C=40 worked well
 const int C=15;
@@ -15,50 +13,8 @@ const int H=700;
 int tim=250;           // 1 sec = 1000tim
 int mat[R][C];
 bool fresh=false;
-bool over=false;
-bool dark=true;
-bool lines=false;
 
 void menu();
-
-void reverseStr(string& str){
-    int n = str.length();
-    for (int i = 0; i < n / 2; i++)
-        swap(str[i], str[n - i - 1]);
-}
-
-string itos(int n){
-    switch(n){
-        case 0: return "0";
-        case 1: return "1";
-        case 2: return "2";
-        case 3: return "3";
-        case 4: return "4";
-        case 5: return "5";
-        case 6: return "6";
-        case 7: return "7";
-        case 8: return "8";
-        case 9: return "9";
-    }
-    return "#";
-}
-
-string getScore(){
-    string board = "Score : ";
-    string ss="";
-    int num=score;
-    if(num!=0){
-        while(num>0){
-            ss.append(itos(num%10));
-            num=num/10;
-        }
-    }else{
-        ss="0";
-    }
-    reverseStr(ss);
-    board.append(ss);
-    return board;
-}
 
 void clearLine(int index){
     for(int i=index;i<R-1;i++){
@@ -79,7 +35,6 @@ void lineClean(){
         }
         if(cleared){
             clearLine(i);
-            score+=1;
             glutPostRedisplay();
         }
     }
@@ -107,40 +62,6 @@ float* color(int x){
         case 6: return c6;
                 break;
         default: return c0;
-    }
-}
-
-void clearMat(){
-    for(int i=0;i<R;i++){
-        for(int j=0;j<C;j++){
-            mat[i][j]=0;
-        }
-    }
-}
-
-void drawString(float x, float y, float z, char *string,int f) {
-    if(dark){
-        glColor3f(1.0,1.0,1.0);
-    }else{
-        glColor3f(0.0,0.0,0.0);
-    }
-    glRasterPos3f(x, y, z);
-    for (char* c = string; *c != '\0'; c++) {
-        if(f==1)
-            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, *c);  // Updates the position
-        else if(f==2)
-            glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, *c);
-    }
-}
-
-void drawString(float x, float y, float z, char *string,int f,float r,float g,float b) {
-    glColor3f(r,g,b);
-    glRasterPos3f(x, y, z);
-    for (char* c = string; *c != '\0'; c++) {
-        if(f==1)
-            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, *c);  // Updates the position
-        else if(f==2)
-            glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, *c);
     }
 }
 
@@ -450,12 +371,6 @@ class block{
                 return true;
             }
             else{
-                clearMat();
-                outClean();
-                cleanBlock();
-                fresh=true;
-                over=true;
-                paused=true;
                 return false;
             }
         }
@@ -487,6 +402,14 @@ void test(){
     }
 }
 
+void clearMat(){
+    for(int i=0;i<R;i++){
+        for(int j=0;j<C;j++){
+            mat[i][j]=0;
+        }
+    }
+}
+
 void matInit(){
     for(int i=0;i<R;i++){
         for(int j=0;j<C;j++){
@@ -496,11 +419,7 @@ void matInit(){
 }
 
 void init(){
-    if(dark){
-        glClearColor(0.0,0.0,0.0,1.0);
-    }else{
-        glClearColor(1.0,1.0,1.0,1.0);
-    } // BGColor
+    glClearColor(0.0,0.0,0.0,1.0);  // BGColor
 }
 
 void reshape(int w, int h){
@@ -515,11 +434,8 @@ void reshape(int w, int h){
 
 void border(){
     float x=(float)C,y=(float)R;
-    if(dark)
-        glColor3f(1.0,1.0,1.0);
-    else
-        glColor3f(0.0,0.0,0.0);
-    glLineWidth(3);
+    glColor3f(1.0,1.0,1.0);
+    glLineWidth(2);
     glBegin(GL_LINES);
     glVertex2f(0.0,0.0);
     glVertex2f(x,0.0);
@@ -545,19 +461,7 @@ void mapMatrix(){
                 glVertex2f(x+1,y+1);
                 glVertex2f(x,y+1);
                 glEnd();
-                if(dark){
-                    if(!lines){
-                        glColor3f(0.0,0.0,0.0);
-                    }else{
-                        glColor3f(1.0,1.0,1.0);
-                    }
-                }else{
-                    if(!lines){
-                        glColor3f(1.0,1.0,1.0);
-                    }else{
-                        glColor3f(0.0,0.0,0.0);
-                    }
-                }
+                glColor3f(0.0,0.0,0.0);
                 glLineWidth(2);
                 glBegin(GL_LINES);
                 glVertex2f(x,y);
@@ -574,35 +478,10 @@ void mapMatrix(){
     }
 }
 
-void scoreBoard(){
-    glColor3f(1.0,1.0,1.0);
-    string board=getScore();
-    int n=board.length();
-    char board_array[n + 1];
-    strcpy(board_array, board.c_str());
-    drawString(11.0,R-1.0,0.0,board_array,2);
-}
-
 void display(void){
     glClear(GL_COLOR_BUFFER_BIT );
     glLoadIdentity();
-    if(dark){
-        glClearColor(0.0,0.0,0.0,1.0);
-    }else{
-        glClearColor(1.0,1.0,1.0,1.0);
-    }
-    if(!over){
-        mapMatrix();
-        scoreBoard();
-    }else{
-        char over[]="GAME OVER!!!";
-        string board=getScore();
-        int n=board.length();
-        char board_array[n + 1];
-        strcpy(board_array, board.c_str());
-        drawString((C/2)-3.0,(R/2)+2.0,0.0,over,1);
-        drawString((C/2)-1.25,(R/2)-2.0,0.0,board_array,1);
-    }
+    mapMatrix();
     border();
     glutSwapBuffers();
 }
@@ -626,7 +505,7 @@ void timer(int){
             t=1;
         }
         if(!b.process()){
-            if(b.x<R){
+            if(b.x<R-4){
                 b.outClean();
                 b.blockInit();
             }
@@ -650,21 +529,6 @@ void keyboard(unsigned char c,int x,int y){
             //glutSetWindow(menuInt);
             menu();
         break;
-        case 'D':
-        case 'd':
-            if(dark){
-                dark=false;
-            }else{
-                dark=true;
-            }
-            break;
-        case 'L':
-        case 'l':
-            if(lines){
-                lines=false;
-            }else{
-                lines=true;
-            }
     }
 }
 
@@ -695,7 +559,6 @@ void specialFunc(int Key, int x, int y){
 }
 
 void game(){
-    over=false;
     glutInitWindowSize(W,H);
     glutInitWindowPosition(200,0);
     glutCreateWindow("Tetris");
@@ -706,28 +569,29 @@ void game(){
     glutKeyboardFunc(keyboard);
     glutTimerFunc(0,timer,0);
     init();
-    glutMainLoop();
+    glutMainLoop();  
+}
+
+void drawString(float x, float y, float z, char *string) {
+    glRasterPos3f(x, y, z);
+    for (char* c = string; *c != '\0'; c++) {
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, *c);  // Updates the position
+    }
 }
 
 void drawButton(){
-    if(dark){
-        glColor3f(1.0,1.0,1.0);
-    }else{
-        glColor3f(0.0,0.0,0.0);
-    }
+    glColor3f(1.0,1.0,1.0);
     glBegin(GL_QUADS);
     glVertex2f((C/2)-2,(R/2)-1);
     glVertex2f((C/2)+3,(R/2)-1);
     glVertex2f((C/2)+3,(R/2)+2);
     glVertex2f((C/2)-2,(R/2)+2);
     glEnd();
+    glColor3f(0.0,0.0,0.0);
     char play[]="PLAY";
-    if(dark)
-        drawString((C/2)-0.8,(R/2)+0.2,0.0,play,1,0.0,0.0,0.0);
-    else
-        drawString((C/2)-0.8,(R/2)+0.2,0.0,play,1,1.0,1.0,1.0);
+    drawString((C/2)-0.8,(R/2)+0.2,0.0,play);
 }
-
+    
 bool inButton(int x,int y){
     if(x>=118&&x<=232){
         if(y>=304&&y<=370){
@@ -753,19 +617,14 @@ void writeCredits(){
     char by[]="Developed By :";
     char one[]="Sahan KJ : 4SN18CS076";
     char two[]="Shibin Joseph : 4SN18083";
-    drawString(3.0,R-2.0,0.0,oglt,1);
-    drawString(5.9,R-12.0,0.0,title,1);
-    drawString(4.5,5.5,0.0,by,1);
-    drawString(2.3,4.0,0.0,one,1);
-    drawString(2.0,2.5,0.0,two,1);
+    drawString(3.0,R-2.0,0.0,oglt);
+    drawString(5.9,R-12.0,0.0,title);
+    drawString(4.5,5.5,0.0,by);
+    drawString(2.3,4.0,0.0,one);
+    drawString(2.0,2.5,0.0,two);
 }
 
 void displayMenu(void){
-    if(dark){
-        glClearColor(0.0,0.0,0.0,1.0);
-    }else{
-        glClearColor(1.0,1.0,1.0,1.0);
-    }
     glClear(GL_COLOR_BUFFER_BIT );
     glLoadIdentity();
     border();
@@ -798,19 +657,9 @@ void keyboardMenu(unsigned char c,int x,int y){
                 b.blockInit();
                 game();
             }
-            break;
+        break;
         case '\e':
             glutDestroyWindow(glutGetWindow());
-            break;
-        case 'D':
-        case 'd':
-            if(dark){
-                dark=false;
-            }else{
-                dark=true;
-            }
-            glutPostRedisplay();
-            break;
     }
 }
 
@@ -826,7 +675,7 @@ void menu(){
     glutKeyboardFunc(keyboardMenu);
     glutMouseFunc(mouseCall);
     init();
-    glutMainLoop();
+    glutMainLoop();  
 }
 
 int main(int argc,char** argv){
@@ -836,6 +685,5 @@ int main(int argc,char** argv){
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE );
     menu();
-    return 0;
+    return 0; 
 }
-
